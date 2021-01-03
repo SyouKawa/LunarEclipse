49c07ef75cad36662afcc0e8834479bed58081d9
c6h12yith <chernoar_syoukawa_bisyoufish@outlook.com>
2021-01-03 15:26:29 +0800

    简易StartGame视差

--- LEGame/Assets/Scripts/Effects/SimpleMouseBGEffects.cs
+++ LEGame/Assets/Scripts/Effects/SimpleMouseBGEffects.cs
@@ -4,19 +4,20 @@ using UnityEngine;
 
 public class SimpleMouseBGEffects : MonoBehaviour
 {
-    public float XLimit = 1.5f;
-    public float YLimit = 0.3f;
+    public float XLimit;
+    public float YLimit;
     public int middleXAxis = 1280;
-    public int middleYAxis = 720;
+    //public int middleYAxis = 720;
     private Vector3 newPos = Vector3.zero;
 
     void UpdateMovedPos()
     {
+        newPos.z = transform.position.z;
         float tempX = Input.mousePosition.x - middleXAxis;
-        newPos.x = (-tempX / middleXAxis) * XLimit;
+        newPos.x = (tempX / middleXAxis) * XLimit;
 
-        float tempY = Input.mousePosition.y - middleYAxis;
-        newPos.y = (-tempY / middleYAxis) * YLimit;
+        /*float tempY = Input.mousePosition.y - middleYAxis;
+        newPos.y = (tempY / middleYAxis) * YLimit;*/
         
     }
 
@@ -25,7 +26,8 @@ public class SimpleMouseBGEffects : MonoBehaviour
         UpdateMovedPos();
         if (Mathf.Abs(newPos.x) < XLimit && Mathf.Abs(newPos.y) < YLimit)
         {
-            transform.position = Vector3.Lerp(transform.position, newPos, Time.deltaTime);
+            transform.position = newPos;
+            //transform.position = Vector3.Lerp(transform.position, newPos, Time.deltaTime);
         }
     }
 }
