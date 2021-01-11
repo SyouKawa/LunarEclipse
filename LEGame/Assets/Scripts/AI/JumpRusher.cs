f131117d9f8d94d4e3a8ac52caf1a5d748370f08
c6h12yith <chernoar_syoukawa_bisyoufish@outlook.com>
2021-01-11 21:12:48 +0800

    跳跃AI v0.1

--- LEGame/Assets/Scripts/AI/JumpRusher.cs
+++ LEGame/Assets/Scripts/AI/JumpRusher.cs
@@ -14,31 +14,73 @@ public class JumpRusher : MonoBehaviour
     public float HP;
     public Status curState;
     public float angle;
-    private bool isGrounded;
+    public bool isGrounded;
     public float jumpInitSpeed;
     public Vector2 gravity = new Vector2(0,-10f);
     public Vector2 velocity = Vector2.zero;
     public GameObject tempPlayer;
 
     public float rangeofPatrol;
-    public bool isLock;
+    public float checkline;
     public bool isHit;
-
+    public bool isLock;
     private BoxCollider2D body;
 
 
     void Start()
     {
         isGrounded = true;
+        body = GetComponent<BoxCollider2D>();
     }
 
     void DestroySelf(){}
 
+    void CheckGround()
+    {
+        //RaycastHit2D hit = Physics2D.Raycast(transform.position, -1*Vector3.up,3f,7);
+        LayerMask mask = LayerMask.GetMask("Ground");
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, -Vector2.up,checkline,mask);
+        Debug.Log(hit.collider);
+        //接触到地面层，且脚下有东西（防止是墙面）是否在地面上判定
+        if(hit.collider != null)
+        {
+            isGrounded = true;
+            if(!isLock)
+            {
+                velocity = Vector2.zero;
+            }
+        }
+        else
+        {
+            isGrounded = false;
+            velocity += gravity *Time.deltaTime;
+            Vector2 pos = (Vector2)transform.position + velocity* Time.deltaTime;
+            transform.position = pos;
+        }
+    }
+
+    void Jump()
+    {
+        isLock = true;
+        Vector2 dir = Vector2.zero;
+        if(transform.position.x - tempPlayer.transform.position.x > 0)
+        {
+            velocity.x = -3;
+        }
+        else
+        {
+            velocity.x = 3;
+        }
+        //给予上升的初始速度                            
+        velocity.y += jumpInitSpeed;
+    }
+
     // Update is called once per frame
     void Update()
     {
-        if(body.IsTouchingLayers("Ground"){}
-        
+        //循环检测是否在地面上
+        CheckGround();
+        //附近是否存在玩家
         if(tempPlayer == null)
         {
             curState = Status.Patrol;
@@ -47,6 +89,7 @@ public class JumpRusher : MonoBehaviour
         //检测到有玩家时的AI
         else
         {
+            //死亡判定
             if(HP <= 0)
             {
                 curState = Status.Die;
@@ -56,31 +99,12 @@ public class JumpRusher : MonoBehaviour
             }
             //按距离条件判定：是否进入对玩家的攻击状态
             float distance = Vector2.Distance(tempPlayer.transform.position,transform.position);
-            if(distance < rangeofPatrol)
+            if(!isLock && distance < rangeofPatrol)
             {
                 //进玩家入攻击范围，且未在做其他动作，将巡逻状态变为“接近玩家”
                 curState = Status.Approch;
                 //处理
-                if(isGrounded)
-                {
-                    Vector2 dir = Vector2.zero;
-                    if(transform.position.x - tempPlayer.transform.position.x > 0)
-                    {
-                        velocity.x = -1;
-                    }
-                    else
-                    {
-                        velocity.x = 1;
-                    }
-                    //给予上升的初始速度                            
-                    velocity.y += jumpInitSpeed;
-                }
-                else
-                {
-                    //每秒受重力作用减少速度
-                    velocity += gravity *Time.deltaTime;
-                    Vector2 pos = (Vector2)transform.position + velocity* Time.deltaTime;
-                }
+                Jump();
             }
             else
             {
@@ -93,5 +117,6 @@ public class JumpRusher : MonoBehaviour
     {
         Gizmos.color = Color.red;
         Gizmos.DrawWireCube(transform.position,new Vector3(rangeofPatrol,3,2));
+        Gizmos.DrawRay(transform.position,checkline*Vector2.up);
     }
 }
