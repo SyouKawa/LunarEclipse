8650deae266ae763d664d856a1a94b299ec6fe3b
xc6zyith <orekiyuki2me@gmail.com>
2021-03-09 00:32:44 +0800

    简易的DebugPanel外观 v1.5

--- LEGame/Assets/Scripts/Tools/SEDebug.cs
+++ LEGame/Assets/Scripts/Tools/SEDebug.cs
@@ -17,16 +17,10 @@ public class SEDebug : MonoBehaviour
 
     //Debug信息框的宽度和高度
     public int boxWidth = 400;
-    public int boxHeight = 590;
-    //Debug Button大小设置
-    public int btnWidth = 250;
-    public int btnHeight = 50;
+    public int boxHeight = 450;
     //Debug Label大小设置
     public int lblWidth = 360;
     public int lblHeight = 50;
-    //Debug toggle大小设置
-    public int tglWidth = 250;
-    public int tglHeight = 50;
 
     [Header("Debug信息配置")]
     public string msgStake;
@@ -52,8 +46,9 @@ public class SEDebug : MonoBehaviour
         GUI.skin = skin;
         //左侧面板背景配置
         GUI.Box(new Rect(marginLeft,marginTop,boxWidth,boxHeight),"信息显示");
-        GUI.BeginGroup(new Rect(marginLeft,marginTop,boxWidth,boxHeight));
-        if(GUI.Button(new Rect(padding,padding*2,btnWidth,btnHeight),"刷新场景物体到管理器"))
+        GUILayout.BeginArea(new Rect(marginLeft+padding,marginTop+padding,boxWidth,boxHeight));
+        GUILayout.BeginVertical();
+        if(GUILayout.Button("刷新场景物体到管理器",GUILayout.Height(50),GUILayout.Width(250)))
         {
             FreshObjects();
         }
@@ -66,25 +61,27 @@ public class SEDebug : MonoBehaviour
         {
             Debug.LogWarning("切换场景后，请点击“刷新场景物体到管理器”按钮，防止Player物体为空");
         }
-        GUI.Label(new Rect(padding,padding*4,lblWidth,lblHeight),"当前速度："+ velocity.ToString());
-        GUI.Toggle(new Rect(padding ,padding*4+tglHeight,tglWidth,tglHeight),player.IsCollidingGround,"是否站在地上");
-        GUI.Toggle(new Rect(padding ,padding*4+tglHeight*2,tglWidth,tglHeight),player.isJumping,"是否在跳跃");
-        GUI.Toggle(new Rect(padding ,padding*4+tglHeight*3,tglWidth,tglHeight),player.isAttack,"是否在攻击");
-        GUI.Label(new Rect(padding,padding*4 +tglHeight*3+lblHeight,lblWidth,lblHeight),"跳跃速度（上升）："+ player.jumpInitSpeed.ToString());
-        GUI.Label(new Rect(padding,padding*4 +tglHeight*3+lblHeight*2,lblWidth,lblHeight),"下落速度（下坠）："+ player.jumpDownSpeed.ToString());
-        GUI.Label(new Rect(padding,padding*4 +tglHeight*3+ lblHeight *3,lblWidth,lblHeight),"最大跳跃高度："+ player.MaxHeight.ToString());
-        GUI.Label(new Rect(padding,padding*4 +tglHeight*3+ lblHeight *4,lblWidth,lblHeight),"起跳位置（Y轴）："+ player.initYpos.ToString());
-        GUI.Label(new Rect(padding,padding*4 +tglHeight*3+ lblHeight *5,lblWidth,lblHeight),"跳跃的目标位置（Y轴）："+ player.targetYPostion.ToString());
-        GUI.EndGroup();
+        GUILayout.Label("当前速度："+ velocity.ToString());
+        GUILayout.Toggle(player.IsCollidingGround,"是否站在地上");
+        GUILayout.Toggle(player.isJumping,"是否在跳跃");
+        GUILayout.Toggle(player.isAttack,"是否在攻击");
+        GUILayout.Label("跳跃速度（上升）："+ player.jumpInitSpeed.ToString());
+        GUILayout.Label("下落速度（下坠）："+ player.jumpDownSpeed.ToString());
+        GUILayout.Label("最大跳跃高度："+ player.MaxHeight.ToString());
+        GUILayout.Label("起跳位置（Y轴）："+ player.initYpos.ToString());
+        GUILayout.Label("跳跃的目标位置（Y轴）："+ player.targetYPostion.ToString());
+        
+        GUILayout.EndVertical();
+        GUILayout.EndArea();
 
         // 右侧面板背景配置
-        GUI.Box(new Rect(Screen.width-boxWidth,marginTop,boxWidth,boxHeight), "加载关卡");
+        GUI.Box(new Rect(Screen.width-boxWidth,marginTop,boxWidth,100), "加载关卡");
 
         GUI.Label(new Rect(Screen.width-boxWidth+padding,marginTop+padding,lblWidth,lblHeight),"前往关卡：");
-        levelInput = GUI.TextField(new Rect(Screen.width-boxWidth+150,marginTop+padding,btnHeight,btnHeight),levelInput);
+        levelInput = GUI.TextField(new Rect(Screen.width-boxWidth+150,marginTop+padding,50,50),levelInput);
         
         // 创建第一个按钮。如果按下此按钮，则会执行 Application.Loadlevel (1)
-        if(GUI.Button(new Rect(Screen.width-boxWidth+250,marginTop + padding,btnWidth,btnHeight), "确认"))
+        if(GUI.Button(new Rect(Screen.width-boxWidth+250,marginTop + padding,140,50), "确认"))
         {
             try
             {
