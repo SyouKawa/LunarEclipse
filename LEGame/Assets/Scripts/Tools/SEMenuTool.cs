6cb8f80a24acb8ea81cdb443aac34da7647dbf42
xc6zyith <orekiyuki2me@gmail.com>
2021-03-09 05:08:32 +0800

    添加了SEGame工具栏，辅助创建部分游戏物体

---
+++ LEGame/Assets/Scripts/Tools/SEMenuTool.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class SEMenuTool : MonoBehaviour
+{
+    [MenuItem("SEGame/Add GameManager")]
+    public static void AddGameManagerToCurScene()
+    {
+        if(GameObject.FindWithTag("Manager") == null)
+        {
+            GameManager MgrOrigin = Resources.Load<GameManager>("Prefabs/GameManager");
+            GameObject.Instantiate(MgrOrigin,Vector3.zero,Quaternion.identity);
+        }
+        else
+        {
+            Debug.Log("当前场景中已存在GameManager，请勿重复创建");
+        }
+    }
+
+    [MenuItem("SEGame/Add Player")]
+    public static void AddPlayerToCurScene()
+    {
+        if(GameObject.FindWithTag("Player") == null)
+        {
+            Player origin = Resources.Load<Player>("Prefabs/Player");
+            GameObject.Instantiate(origin,Vector3.zero,Quaternion.identity);
+        }
+        else
+        {
+            Debug.Log("当前场景中已存在Player，请勿重复创建");
+        }
+    }
+
+}
